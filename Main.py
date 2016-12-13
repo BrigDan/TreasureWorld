77114f4faa2227f139cd4d1f5da6c37e8fe4405f
JakubOlender <jakub.olender@adacollege.org.uk>
2016-12-13 09:50:28 +0000

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,9 +1,14 @@
 # Treasure World Game
 
 import random
-
+win = True
+gold = 0
+lives = 3
+turnsOfOmnisense = 0
+Censor = True
 xLocation = 0
 yLocation = 0
+random_or_not = 0
 
 def load_grid():
     Grid = []
@@ -13,9 +18,19 @@ def load_grid():
         lines = lines.strip("\n")            # Discard \n from the lines
         list_of_lines = list(lines)          # Separates each character in each line
         Grid.append(list_of_lines)           # Put every list into another list
+        file.close()
     return Grid
 
-#print(load_grid())
+
+def event(world,x,y,gold,lives,censor):
+    if world[x][y] == "G":
+        gold += 1
+    elif world[x][y] == "S":
+        lives -=1
+    elif world[x][y] == "U":
+        censor += 3
+    return gold,lives,censor
+
 
 def Random_grid(height, length):
     Grid = []
@@ -37,7 +52,7 @@ def Random_grid(height, length):
 def reveal_grid(Grid, censor):
     for x in range(len(Grid)):               # len(Grid) counts amount of items in Grid and x is from zero to amount og items in grid
         for y in range(len(Grid)):
-            if censor:                       # If sensor is true
+            if censor:                       # If censor is true
                 print("~", end = "")         # All should be '~'
             else:
                 print(Grid[x][y], end = "")  # Otherwise print the actual Grid
@@ -47,51 +62,80 @@ def PlayerLocation(Grid,xlocation,ylocation):
     Grid[xlocation][ylocation] = "#"
     return Grid
 
-random_or_not = int(input("Load grid from txt file (Enter 1)\n"
-                          "and a randomly generated grid (enter 2)\n"
-                          "=>"))
-lives = 3
+
+
+while random_or_not != 1 or 2:
+    try:
+        random_or_not = int(input("Load grid from txt file (Enter 1)\n"
+                                  "and a randomly generated grid (enter 2)\n"
+                                  "> "))
+    except:
+        print("Wrong input. Please enter again.\n")
+
+
+
 if random_or_not == 1:
     world = load_grid()
     world = PlayerLocation(world,xLocation,yLocation)
     reveal_grid(world, True)
     print()
-    reveal_grid(world, False)
-    while lives > 0:
-        world = PlayerLocation(world,xLocation,yLocation)
-        print("the player is at",xLocation,",",yLocation,)
-        #lives -= 1
-        movement=input("Which way do you want to go?").lower()
-        #RightInput = False
-        if movement == "w":
-            world[xLocation][yLocation]="*"
-            xLocation -= 1
-            if xLocation < 0:
-                xLocation +=1
-        elif movement == "s":
-            world[xLocation][yLocation] = "*"
-            xLocation += 1
-            if xLocation >= 10:
-                xLocation -=1
-        elif movement == "d":
-            world[xLocation][yLocation] = "*"
-            yLocation += 1
-            if yLocation >= 10:
-                yLocation -=1
-        elif movement == "a":
-            world[xLocation][yLocation] = "*"
-            yLocation -= 1
-            if yLocation < 0:
-                yLocation +=1
-        else:
-            print("Wrong input! Try again")
-        world= PlayerLocation(world,xLocation,yLocation)
-        print()
-        reveal_grid(world, True)
-        print()
-        reveal_grid(world, False)
+
+
+
 elif random_or_not == 2:
     world = Random_grid(10, 10)                # Randomly generates a 10x10 grid
     reveal_grid(world, True)                 # Prints the grid censored
     print()
-    #reveal_grid(world, False)
+
+
+while lives > 0:
+    world = PlayerLocation(world,xLocation,yLocation)
+    print("The player is at",xLocation,",",yLocation,)
+    movement=input("Which way do you want to go?\n> ").lower()
+    if movement == "w":
+        world[xLocation][yLocation]="*"
+        xLocation -= 1
+    if xLocation < 0:
+        xLocation +=1
+    elif movement == "s":
+        world[xLocation][yLocation] = "*"
+        xLocation += 1
+    if xLocation >= 10:
+        xLocation -=1
+    elif movement == "d":
+        world[xLocation][yLocation] = "*"
+        yLocation += 1
+    if yLocation >= 10:
+        yLocation -=1
+    elif movement == "a":
+        world[xLocation][yLocation] = "*"
+        yLocation -= 1
+    if yLocation < 0:
+        yLocation +=1
+    elif movement == "stats()":
+            print("Gold:",gold,"\nLives:",lives,"\nTurns of Omnisense left:",turnsOfOmnisense)
+    elif movement == "debug()":
+            turnsOfOmnisense+=3
+    else:
+        print("Wrong input! Try again")
+    gold,lives,turnsOfOmnisense=event(world,xLocation,yLocation,gold,lives,turnsOfOmnisense)
+    world= PlayerLocation(world,xLocation,yLocation)
+    if turnsOfOmnisense > 0:
+        Censor = True
+        turnsOfOmnisense-=1
+        print("good")
+    print()
+    reveal_grid(world, Censor)
+    print()
+    if turnsOfOmnisense == 0:
+        Censor = True
+    if gold == 3:
+        break
+
+
+if lives == 0:
+    win = False
+if win:
+    print("You win")
+else:
+    print("You lose")
